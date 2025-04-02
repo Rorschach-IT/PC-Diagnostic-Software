4d98698f5ee3e23ab8b53718e8a4ac44869b2f35
JakubP <j.poczatekrorschach@gmail.com>
2025-04-02 02:05:14 +0200

    design change, button hoover effect and refactoring

--- src/PC Diagnostic Software/PC Scan App/MainWindow.xaml.cs
+++ src/PC Diagnostic Software/PC Scan App/MainWindow.xaml.cs
@@ -1,13 +1,9 @@
 ﻿using System.Windows;
-using System.Windows.Media;
 
 namespace PC_Scan_App
 {
     public partial class MainWindow : Window
     {
-        private readonly Brush hoverBackground = (Brush)new BrushConverter().ConvertFromString("#EEDEDE");
-        private readonly Brush defaultBackground = (Brush)new BrushConverter().ConvertFromString("#FFFFFF");
-
         public MainWindow()
         {
             InitializeComponent();
